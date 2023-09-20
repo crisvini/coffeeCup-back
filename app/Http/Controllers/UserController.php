cfbae859892dcd10155421f36d122a0d30842162
crisvini <crisvini.leoncini@outlook.com>
2023-09-20 15:59:47 -0300

    user crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,61 +7,30 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return 'teste';
+        return User::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $user = new User(request()->all());
-        return $user;
-        // $user->name = $request->name;
+        User::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        return User::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->update($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::get('/user', [UserController::class, 'index']);
-// Route::post('/user/store', [UserController::class, 'store']);
