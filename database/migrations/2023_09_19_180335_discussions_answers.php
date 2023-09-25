e0288827070535fad00223ac5df22b7a1ae984e1
crisvini <crisvini.leoncini@outlook.com>
2023-09-25 15:53:02 -0300

    correção migration

--- database/migrations/2023_09_19_180335_discussions_answers.php
+++ database/migrations/2023_09_19_180335_discussions_answers.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('discussions_answers', function (Blueprint $table) {
             $table->id();
-            $table->text('texto');
+            $table->text('text');
             $table->unsignedBigInteger('discussion_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
