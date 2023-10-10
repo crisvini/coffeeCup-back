1fedfaa69bf75aceeb2d70f306b7eeb78b800209
crisvini <crisvini.leoncini@outlook.com>
2023-10-10 18:46:22 -0300

    correção rota store users

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
     Route::post('/login', 'login');
-    Route::post('/users', 'store');
+    Route::post('/users/store', 'store');
     Route::post('/send-verification-token', 'sendVerificationToken');
     Route::get('/receive-verification-token/{id}', 'receiveVerificationToken');
 });
