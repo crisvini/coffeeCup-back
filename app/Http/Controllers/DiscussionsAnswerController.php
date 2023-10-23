518fc0a9cd51753157242bb16acb169444b30426
crisvini <crisvini.leoncini@outlook.com>
2023-10-23 14:48:17 -0300

    implementação de respostas de discussões

--- app/Http/Controllers/DiscussionsAnswerController.php
+++ app/Http/Controllers/DiscussionsAnswerController.php
@@ -4,22 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\DiscussionsAnswer;
 use Illuminate\Http\Request;
+use Mockery\CountValidator\Exception;
 
 class DiscussionsAnswerController extends Controller
 {
     public function index()
     {
-        return DiscussionsAnswer::all();
+        return DiscussionsAnswer::with('user:id,name,email')->orderBy('created_at', 'desc')->get();
     }
 
     public function store(Request $request)
     {
-        DiscussionsAnswer::create($request->all());
+        try {
+            $answer = DiscussionsAnswer::create($request->all());
+            $answer->load('user:id,name,email');
+        } catch (Exception  $e) {
+            return false;
+        }
+        return response()->json($answer, 200);
     }
 
     public function show(string $id)
     {
-        return DiscussionsAnswer::findOrFail($id);
+        return DiscussionsAnswer::with('user:id,name,email')->findOrFail($id);
     }
 
     public function update(Request $request, string $id)
